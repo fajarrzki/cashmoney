cbb38b0a340d26ce87f865459b740d4d37bd9dc9
fajarrzki <fajarapriorizky@gmail.com>
2024-06-06 10:56:50 +0700

    first commit

---
+++ Readme.txt
@@ -0,0 +1,17 @@
+Cashmoneycleaning Login account
+
+admin:
+1. user:admin
+   pass:password
+
+2. user:rizqiadmin
+   pass:rizqiadmin
+
+kurir
+1. user:fajar
+   pass:fajar
+
+customer:
+1. user:mahen
+   pass:mahenmahen
+
---
+++ index.php
@@ -0,0 +1,491 @@
+<!DOCTYPE html>
+<html lang="zxx">
+
+<head>
+    <!-- meta tag -->
+    <meta charset="utf-8">
+    <title>Cashmoneycleaning Services</title>
+    <meta name="description" content="">
+    <!-- responsive tag -->
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- favicon -->
+    <link rel="apple-touch-icon" href="apple-touch-icon.png">
+    <link rel="shortcut icon" type="image/x-icon" href="assets/images/iconcmc.png">
+    <!-- Bootstrap v4.4.1 css -->
+    <link rel="stylesheet" type="text/css" href="assets/css/bootstrap.min.css">
+    <!-- font-awesome css -->
+    <link rel="stylesheet" type="text/css" href="assets/css/font-awesome.min.css">
+    <!-- Uicons Regular Rounded css -->
+    <link rel="stylesheet" type="text/css" href="assets/css/uicons-regular-rounded.css">
+    <!-- flaticon css -->
+    <link rel="stylesheet" type="text/css" href="assets/fonts/flaticon.css">
+    <!-- animate css -->
+    <link rel="stylesheet" type="text/css" href="assets/css/animate.css">
+    <!-- owl.carousel css -->
+    <link rel="stylesheet" type="text/css" href="assets/css/owl.carousel.css">
+    <!-- off canvas css -->
+    <link rel="stylesheet" type="text/css" href="assets/css/off-canvas.css">
+    <!-- magnific popup css -->
+    <link rel="stylesheet" type="text/css" href="assets/css/magnific-popup.css">
+    <!-- Main Menu css -->
+    <link rel="stylesheet" href="assets/css/rsmenu-main.css">
+    <!-- spacing css -->
+    <link rel="stylesheet" type="text/css" href="assets/css/rs-spacing.css">
+    <!-- style css -->
+    <link rel="stylesheet" type="text/css" href="style.css">
+    <!-- This stylesheet dynamically changed from style.less -->
+    <!-- responsive css -->
+    <link rel="stylesheet" type="text/css" href="assets/css/responsive.css">
+
+    <style>
+        /* aboutStart */
+.tentang {
+  padding: 6.5rem 7% 1.4rem;
+}
+
+.tentang h2 {
+  text-align: center;
+  margin-bottom: 3rem;
+  font-size: 1.5rem;
+}
+
+.tentang .roww {
+  display: flex;
+}
+
+.tentang .roww .about-img {
+  padding: 30px;
+  padding-right: 100px;
+}
+
+.tentang .roww .about-img img {
+  width: 100rem;
+}
+
+.tentang .roww .content-roww {
+  flex: 1 1 25rem;
+  padding: 0 1rem;
+  margin-top: 2rem;
+}
+
+.tentang .roww .content-roww h3 {
+  font-size: 1.8rem;
+  margin-bottom: 1rem;
+}
+
+.tentang .roww .content-roww p {
+  font-size: 15px;
+  margin-bottom: 0.8rem;
+  font-weight: 100;
+}
+
+.services {
+  display: flex;
+  align-items: center;
+  gap: 2rem;
+}
+
+.services .service-judul {
+  text-align: center;
+  margin-bottom: 2rem;
+  font-size: large;
+}
+
+.service-card {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  background-color: #ffffff;
+  padding: 2rem;
+  border-radius: 10px;
+  width: 80%;
+  box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);
+}
+
+.service-card h3 {
+  font-size: 2rem;
+  margin-bottom: 1rem;
+}
+
+.service-card p {
+  font-size: 1.1rem;
+  text-align: center;
+}
+
+.footer {
+  background-color: #f2f2f2;
+  padding: 20px;
+  text-align: center;
+  height: 40vh;
+  margin-top: 2rem;
+}
+
+.footer-container {
+  justify-content: center;
+  display: flex;
+  align-items: center;
+}
+
+.footer-contact {
+  margin-top: 20px;
+}
+
+.footer-contact h3 {
+  font-size: 30px;
+  font-weight: bold;
+  margin-bottom: 10px;
+}
+
+.footer-contact p {
+  font-size: 14px;
+  margin-bottom: 5px;
+}
+
+/*footer end*/
+
+/* aboutend */
+
+    </style>
+
+</head>
+
+<body class="defult-home">
+
+    <div class="offwrap"></div>
+
+    <!-- Main content Start -->
+    <div class="main-content">
+
+        <!--Full width header Start-->
+        <div class="full-width-header">
+            <!--Header Start-->
+            <header id="rs-header" class="rs-header header-style2">
+               
+
+                <!-- Menu Start -->
+                <div class="menu-area menu-sticky">
+                    <div class="container">
+                        <div class="row-table">
+                            <div class="col-cell header-logo">
+                                <div class="logo-area normal-logo">
+                                    <a href="index.php">
+                                        <!-- <img src="assets/images/yellow-logo.png" alt="logo"> -->
+                                    </a>
+                                </div>
+                                <div class="logo-area sticky-logo">
+                                    <a href="index.php">
+                                        <img src="assets/images/cmclogo.png" alt="logo">
+                                    </a>
+                                </div>
+                            </div>
+                            <div class="col-cell">
+                                <div class="rs-menu-area">
+                                    <div class="main-menu">
+                                        <nav class="rs-menu hidden-md">
+                                            <ul class="nav-menu onepage-menu">
+                                                <li class="current-menu-item"><a href="#rs-header">Beranda</a></li>
+                                                <li><a href="#tentang">Tentang</a> </li>
+                                                
+                                                <li><a href="login.php">Masuk</a> </li>
+                                                
+                                            </ul>
+                                            <!-- //.nav-menu -->
+                                        </nav>
+                                    </div>
+                                    <!-- //.main-menu -->
+                                </div>
+                            </div>
+                            <div class="col-cell">
+                                <div class="expand-btn-inner">
+                                    <ul>
+                                        <li class="sidebarmenu-search">
+                                            <a class="hidden-xs rs-search" data-bs-toggle="modal" data-bs-target="#searchModal" href="#">
+                                                <i class="fi fi-rr-search"></i>
+                                            </a>
+                                        </li>
+                                        <li class="nav-link">
+                                            <a id="nav-expander" class="nav-expander bar" href="#">
+                                                <span class="dot-hum"></span>
+                                                <span class="dot-hum"></span>
+                                                <span class="dot-hum"></span>
+                                            </a>
+                                        </li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <!-- Menu End -->
+
+                <!-- Canvas Menu start -->
+                <nav class="right_menu_togle menu-wrap-off  hidden-md">
+                    <div class="close-btn">
+                        <a id="nav-close" class="nav-close">
+                            <div class="line">
+                                <span class="line1"></span>
+                                <span class="line2"></span>
+                            </div>
+                        </a>
+                    </div>
+                    <div class="rs-offcanvas-inner">
+                        <div class="canvas-logo">
+                            <!-- <a href="index.php"><img src="assets/images/logo-dark.png" alt="logo"></a> -->
+                        </div>
+                        <div class="offcanvas-text">
+                            <p>Teknolab quisque placerat vitae lacus ut scelerisque. Fusce luctus odio ac nibh luctus, in porttitor theo lacus egestas etiusto odio data center.</p>
+                        </div>
+                        <div class="canvas-contact">
+                            <div class="address-area">
+                                <div class="address-list">
+                                    <div class="info-icon">
+                                        <i class="flaticon-location"></i>
+                                    </div>
+                                    <div class="info-content">
+                                        <h4 class="title">Address</h4>
+                                        <em>05 kandi BR. New York</em>
+                                    </div>
+                                </div>
+                                <div class="address-list">
+                                    <div class="info-icon">
+                                        <i class="flaticon-email"></i>
+                                    </div>
+                                    <div class="info-content">
+                                        <h4 class="title">Email</h4>
+                                        <em><a href="mailto:support@devsdesign.net">support@devsdesign.net</a></em>
+                                    </div>
+                                </div>
+                                <div class="address-list">
+                                    <div class="info-icon">
+                                        <i class="flaticon-call"></i>
+                                    </div>
+                                    <div class="info-content">
+                                        <h4 class="title">Phone</h4>
+                                        <em>+019988772</em>
+                                    </div>
+                                </div>
+                            </div>
+                            <ul class="social">
+                                <li><a href="#"><i class="fa fa-facebook"></i></a></li>
+                                <li><a href="#"><i class="fa fa-twitter"></i></a></li>
+                                <li><a href="#"><i class="fa fa-pinterest-p"></i></a></li>
+                                <li><a href="#"><i class="fa fa-instagram"></i></a></li>
+                            </ul>
+                        </div>
+                    </div>
+                </nav>
+                <!-- Canvas Menu end -->
+
+
+            </header>
+            <!--Header End-->
+        </div>
+        <!--Full width header End-->
+
+        <!-- Slider Section Start -->
+        <div class="rs-slider slide-home-style2">
+            <div class="container">
+                <div class="row y-middle">
+                    <div class="col-lg-5">
+                        <div class="content-wrap">
+                            <div class="brush-content mb-20">
+                                <img src="assets/images/brush.png" alt="">
+                                <span class="sub-text">Cashmoneycleaning</span>
+                            </div>
+                            <h1 class="title wow fadeInLeft " data-wow-delay="800ms" data-wow-duration="2000ms">
+                                We Serve Your Shoes like We Serve our Brother.
+                            </h1>
+                            <div class="btn-part mt-30">
+                                <a class="readon view-slide" href="login.php">
+                                    <span>Login untuk Memesan Jasa</span>
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-7">
+                        <div class="slide-img">
+                            <div class="rs-carousel owl-carousel" data-loop="true" data-items="1" data-margin="0" data-autoplay="true" data-hoverpause="true" data-autoplay-timeout="5000" data-smart-speed="800" data-dots="true" data-nav="false" data-nav-speed="false" data-center-mode="true" data-mobile-device="1" data-mobile-device-nav="false" data-mobile-device-dots="true" data-ipad-device="1" data-ipad-device-nav="false" data-ipad-device-dots="true" data-ipad-device2="1" data-ipad-device-nav2="false" data-ipad-device-dots2="true" data-md-device="1" data-md-device-nav="false" data-md-device-dots="true">
+                                <div class="slider-item">
+                                    <div class="images-part">
+                                        <img src="assets/images/slider/style1/1.jpeg" alt="Images">
+                                    </div>
+                                </div>
+                                <div class="slider-item">
+                                    <div class="images-part">
+                                        <img src="assets/images/slider/style1/2.jpeg" alt="Images">
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- Slider Section End -->
+
+        <!-- Newsletter Section Start -->
+        <div class="rs-newsletter newsletter-style1 yellow-bg pt-70 pb-74">
+            <div class="container">
+                <div class="sec-title mb-25">
+                    <span class="title title2">
+
+                    </span>
+                </div>
+                <div class="newsletter-form">
+
+
+                </div>
+            </div>
+        </div>
+        <!-- Newsletter Section End -->
+
+
+    </div>
+    <!-- Main content End -->
+<!--about start-->
+<section id="tentang" class="tentang">
+      <h2>Kenapa Cuci sepatu di <span>cashmoneycleaning</span>?</h2>
+
+      <div class="roww">
+        <div class="about-img">
+          <img src="assets/images/about/style2/servicess.png"  alt="Images">
+        </div>
+        <div class="content-row">
+          <p>
+            Cashmoneycleaning adalah usaha yang berdedikasi untuk memberikan
+            layanan treatment sepatu berkualitas di daerah Bekasi.
+          </p>
+          <p>
+            Kami bangga dengan layanan kami yang unggul, dengan fokus utama pada
+            memperbaharui dan merawat sepatu pelanggan kami. Dengan motto 'we
+            serve your shoes like we serve our brother', kami berkomitmen untuk
+            memberikan perlakuan terbaik kepada sepatu Anda.
+          </p>
+          <p>
+            kami menawarkan solusi untuk berbagai masalah sepatu Anda, dari
+            membersihkan secara menyeluruh hingga menghilangkan kuning pada sol
+            sepatu. Kami berusaha untuk memberikan pengalaman terbaik bagi para
+            pelanggan kami, karena kami percaya bahwa sepatu yang terawat dengan
+            baik adalah cerminan dari gaya dan kepribadian Anda.
+          </p>
+        </div>
+      </div>
+    </section>
+    <!--about end-->
+
+    <!-- service start -->
+    <section class="services">
+      <div class="service-card">
+        <div class="card-img">
+          <img src="assets/images/about/style2/fastcleaning.png" alt="" />
+        </div>
+        <h3>Fast Cleaning</h3>
+        <p>
+          Fast Cleaning mengacu pada proses pembersihan yang cepat dan efisien
+          yang dilakukan dalam waktu yang lebih singkat dibandingkan dengan
+          pembersihan mendalam.
+        </p>
+      </div>
+      <div class="service-card">
+        <div class="card-img">
+          <img src="assets/images/about/style2/deepcleaning.png" alt="" />
+        </div>
+        <h3>Deep Cleaning</h3>
+        <p>
+          Deep Cleaning mengacu pada proses pembersihan yang menyeluruh dan
+          intensif yang melibatkan pembersihan setiap sudut dan celah dari
+          sebuah ruang atau objek.
+        </p>
+      </div>
+      <div class="service-card">
+        <div class="card-img">
+          <img src="assets/images/about/style2/unyellowing.png" alt="" />
+        </div>
+        <h3>Unyellowing</h3>
+        <p>
+          Sepatu yang menguning mengacu pada sepatu yang berubah warna menjadi
+          kuning atau cokelat karena terpapar sinar matahari, penuaan, atau
+          faktor lainnya.
+        </p>
+      </div>
+    </section>
+    <!-- service end -->
+
+    <!--footer start-->
+    <footer id="footerr" class="footer">
+      <div class="footer-container">
+        <div class="footer-contact">
+          <h3>Hubungi Kami</h3>
+          <p>Email: cleaningcashmoney@gmail.com</p>
+          <p>Whatsapp: +62 811-7702-605</p>
+          <p>
+            Alamat: Jalan Sultan Ageng Tirtayasa Blok K1 No.7, Marga Mulya,
+            Bekasi Utara, KOTA BEKASI, KEC.BEKASI UTARA, JAWA BARAT, 17142
+          </p>
+        </div>
+      </div>
+    </footer>
+    <!--footer end-->
+
+
+
+
+
+    <!-- start scrollUp  -->
+    <div id="scrollUp">
+        <i class="fi fi-rr-arrow-small-up"></i>
+    </div>
+    <!-- End scrollUp  -->
+
+
+    <!-- Search Modal Start -->
+    <div class="modal fade search-modal" id="searchModal" tabindex="-1">
+        <button type="button" class="close" data-bs-dismiss="modal">
+            <i class="fi fi-rr-cross-small"></i>
+        </button>
+        <div class="modal-dialog modal-dialog-centered">
+            <div class="modal-content">
+                <div class="search-block clearfix">
+                    <form>
+                        <div class="form-group">
+                            <input class="form-control" placeholder="Searching..." type="text">
+                            <button type="submit" value="Search"><i class="fi fi-rr-search"></i></button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- Search Modal End -->
+
+    <!-- modernizr js -->
+    <script src="assets/js/modernizr-2.8.3.min.js"></script>
+    <!-- jquery latest version -->
+    <script src="assets/js/jquery.min.js"></script>
+    <!-- Bootstrap v4.4.1 js -->
+    <script src="assets/js/bootstrap.min.js"></script>
+    <!-- op nav js -->
+    <script src="assets/js/jquery.nav.js"></script>
+    <!-- owl.carousel js -->
+    <script src="assets/js/owl.carousel.min.js"></script>
+    <!-- wow js -->
+    <script src="assets/js/wow.min.js"></script>
+    <!-- Skill bar js -->
+    <script src="assets/js/skill.bars.jquery.js"></script>
+    <!-- imagesloaded js -->
+    <script src="assets/js/imagesloaded.pkgd.min.js"></script>
+    <!-- waypoints.min js -->
+    <script src="assets/js/waypoints.min.js"></script>
+    <script src="assets/js/jquery.easypiechart.min.js"></script>
+    <!-- counterup.min js -->
+    <script src="assets/js/jquery.counterup.min.js"></script>
+    <!-- magnific popup js -->
+    <script src="assets/js/jquery.magnific-popup.min.js"></script>
+    <!-- contact form js -->
+    <script src="assets/js/contact.form.js"></script>
+    <!-- main js -->
+    <script src="assets/js/main.js"></script>
+</body>
+
+</html>
